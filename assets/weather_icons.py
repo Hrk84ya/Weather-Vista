a71276a66e21564623c9e277719003b273479a6e
Harsh Kumar <hrk84ya@gmail.com>
2025-03-01 20:51:23 +0530

    Icons

---
+++ assets/weather_icons.py
@@ -0,0 +1,20 @@
+WEATHER_ICONS = {
+    "01d": "☀️",  # clear sky day
+    "01n": "🌙",  # clear sky night
+    "02d": "⛅",  # few clouds day
+    "02n": "☁️",  # few clouds night
+    "03d": "☁️",  # scattered clouds
+    "03n": "☁️",  # scattered clouds
+    "04d": "☁️",  # broken clouds
+    "04n": "☁️",  # broken clouds
+    "09d": "🌧️",  # shower rain
+    "09n": "🌧️",  # shower rain
+    "10d": "🌦️",  # rain day
+    "10n": "🌧️",  # rain night
+    "11d": "⛈️",  # thunderstorm
+    "11n": "⛈️",  # thunderstorm
+    "13d": "🌨️",  # snow
+    "13n": "🌨️",  # snow
+    "50d": "🌫️",  # mist
+    "50n": "🌫️",  # mist
+}
